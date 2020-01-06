767b2bea046233efd7c0b2d15bf416fe735a979e
Alexander Pimenov <pimalex1978@yandex.ru>
2020-01-06 21:38:52 +0300

    Урок 7

--- Lesson7.Scopes/src/main/java/ru/alishev/springcourse/TestSpring.java
+++ Lesson7.Scopes/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -6,29 +6,47 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Neil Alishev
  */
 public class TestSpring {
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+		//Т.к. в aplicationContext.xml указан scope="prototype", то Спринг создает разные бины.
+		//Вытащим из контекста объект MusicPlayer, сохраним его в переменную.
+		MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+		//Вытащим из контекста еще один объект MusicPlayer, сохраним его так же в переменную.
+		MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(comparison);
+		// Сравним два объекта MusicPlayer firstMusicPlayer и secondMusicPlayer
+		// убедимся, что это два разных объекта.
+		// на выходе получим false.
+		// Сравнение "==" происходит по указателям на объект: т.е. проверяем указывают
+		// ли два этих
+		// указателя на один и тот же участок памяти.
+		//
+		// Если сравнивать по equals(), то произойдет сравнение по содержанию!!!
 
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+		boolean comparison = firstMusicPlayer == secondMusicPlayer;
 
-        firstMusicPlayer.setVolume(10);
+		System.out.println(comparison);// выводим результат сравнения на экран.
 
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+		//напечатаем наш первый объект (выведется хеш объекта)
+		System.out.println(firstMusicPlayer);
+		
+		//напечатаем наш второй объект (выведется хеш объекта)
+		System.out.println(secondMusicPlayer);
 
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
+		//Изменим громкость первого плеера. При этом у второго плеера она не изменится.
+		firstMusicPlayer.setVolume(10);
 
-        context.close();
-    }
+		//напечатаем 
+		System.out.println(firstMusicPlayer.getVolume());
+		
+		//напечатаем 
+		System.out.println(secondMusicPlayer.getVolume());
+
+		// System.out.println(musicPlayer.getName());
+		// System.out.println(musicPlayer.getVolume());
+
+		context.close();
+	}
 }
