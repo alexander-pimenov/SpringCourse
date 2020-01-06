84c20ae05597182409ce8647008191602eb550eb
Alexander Pimenov <pimalex1978@yandex.ru>
2020-01-06 21:39:18 +0300

    Урок 8

--- Lesson8.InitDestroyAndFactory/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ Lesson8.InitDestroyAndFactory/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -4,22 +4,43 @@ package ru.alishev.springcourse;
  * @author Neil Alishev
  */
 public class ClassicalMusic implements Music {
-    private ClassicalMusic() {}
 
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
+	// Чтоб продемонстрировать работу метода factory-method
+	// Изменим класс ClassicalMusic так, чтоб пользователь не смог создать
+	// класс с помощью ключевого слова new.
+	// Для этого сделаем приватный конструктор.
+	// И для создания объектов этого класса будем использовать
+	// фабричный метод, в этом и заключается паттерн фабричного метода.
+	private ClassicalMusic() {
+	}
 
-    public void doMyInit() {
-        System.out.println("Doing my initialization");
-    }
+	// Создаем фабричный метод для создания объектов ClassicalMusic.
+	// Он статический, т.е. вызывается на классе, а не на объекте.
+	// Если он НЕ будет статическим, то будет замкнутый круг:
+	// не будет объекта на котором мы можем вызвать фабричный метод,
+	// а единственный способ вызвать объект - это вызвать фабричный метод!!!
+	// Вызвав этот метод, пользователь получит объект того класса, который
+	// ему нужен.
+	public static ClassicalMusic getClassicalMusic() {
+		return new ClassicalMusic();
+	}
 
-    public void doMyDestroy() {
-        System.out.println("Doing my destruction");
-    }
+	//// Добавим инит-метод (init-method)
+	// doMyInit - это любое название.
+	// Это название потом прописываем в файле applicationContext.xml
+	// Здесь хотим, чтобы при создании бина, в консоль выводилось сообщение.
+	public void doMyInit() {
+		System.out.println("Doing my initialization");
+	}
 
-    @Override
-    public String getSong() {
-        return "Hungarian Rhapsody";
-    }
+	//// Добавим дестрой-метод (destroy-method)
+	public void doMyDestroy() {
+		System.out.println("Doing my destruction");
+	}
+
+	@Override
+	public String getSong() {
+		return "Hungarian Rhapsody";
+	}
+
 }
--- Lesson8.InitDestroyAndFactory/src/main/java/ru/alishev/springcourse/TestSpring.java
+++ Lesson8.InitDestroyAndFactory/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -6,17 +6,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Neil Alishev
  */
 public class TestSpring {
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+		// Получаем доступ к бину из контекста, для класса ClassicalMusic
+		ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+		
+		////Создали дополнительные classicalMusic, чтобы показать что при scope=singleton, который
+		//стоит по-умолчанию, создается только один объект класса ClassicalMusic.
+		//classicalMusic и classicalMusic2 и classicalMusic3 будут ссылаться на один и тот же объект.
+		//ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+		//ClassicalMusic classicalMusic3 = context.getBean("musicBean", ClassicalMusic.class);
 
-        System.out.println(classicalMusic.getSong());
+		//// это пример при указании scope=prototype
+		// чтобы показать сколько бинов создастся
+		//ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+
+		// Вызовем у этого бина метод getSong()
+		System.out.println(classicalMusic.getSong());
+		
+		////Сделали вызовы метода getSong() для переменных classicalMusic2 и 
+		//classicalMusic3, чтоб было видно что этот метод можно вызывать без ограничений.
+		//System.out.println(classicalMusic2.getSong());
+		//System.out.println(classicalMusic3.getSong());
+
+		//// это пример при указании scope=prototype
+		// чтобы показать, что у всех созданных бинов вызовется метод getSong() 
+		//System.out.println(classicalMusic2.getSong());
 
 //        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayerf.class);
 //
 //        boolean comparison = firstMusicPlayer == secondMusicPlayer;
 //
@@ -30,9 +49,9 @@ public class TestSpring {
 //        System.out.println(firstMusicPlayer.getVolume());
 //        System.out.println(secondMusicPlayer.getVolume());
 
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
+		// System.out.println(musicPlayer.getName());
+		// System.out.println(musicPlayer.getVolume());
 
-        context.close();
-    }
+		context.close();
+	}
 }
